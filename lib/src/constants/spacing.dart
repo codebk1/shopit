94a95342115aa8bda41a479cff79b79fb96a57ae
Bartek Kaczmarek <codebkme@gmail.com>
2023-06-27 15:28:13 +0200

    feat: add spacing constants

---
+++ lib/src/constants/spacing.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class Spacing {
+  static const s1 = 1.0;
+  static const s2 = 2.0;
+  static const s4 = 4.0;
+  static const s6 = 6.0;
+  static const s8 = 8.0;
+  static const s10 = 10.0;
+  static const s12 = 12.0;
+  static const s14 = 14.0;
+  static const s16 = 16.0;
+  static const s20 = 20.0;
+  static const s24 = 24.0;
+  static const s28 = 28.0;
+  static const s32 = 32.0;
+  static const s36 = 36.0;
+  static const s40 = 40.0;
+  static const s44 = 44.0;
+  static const s48 = 48.0;
+  static const s56 = 56.0;
+  static const s64 = 64.0;
+}
+
+/// horizontal spacing
+const gapW1 = SizedBox(width: Spacing.s1);
+const gapW2 = SizedBox(width: Spacing.s2);
+const gapW4 = SizedBox(width: Spacing.s4);
+const gapW6 = SizedBox(width: Spacing.s6);
+const gapW8 = SizedBox(width: Spacing.s8);
+const gapW10 = SizedBox(width: Spacing.s10);
+const gapW12 = SizedBox(width: Spacing.s12);
+const gapW14 = SizedBox(width: Spacing.s14);
+const gapW16 = SizedBox(width: Spacing.s16);
+const gapW20 = SizedBox(width: Spacing.s20);
+const gapW24 = SizedBox(width: Spacing.s24);
+const gapW28 = SizedBox(width: Spacing.s28);
+const gapW32 = SizedBox(width: Spacing.s32);
+const gapW36 = SizedBox(width: Spacing.s36);
+const gapW40 = SizedBox(width: Spacing.s40);
+const gapW44 = SizedBox(width: Spacing.s44);
+const gapW48 = SizedBox(width: Spacing.s48);
+const gapW56 = SizedBox(width: Spacing.s56);
+const gapW64 = SizedBox(width: Spacing.s64);
+
+/// vertical spacing
+const gapH1 = SizedBox(height: Spacing.s1);
+const gapH2 = SizedBox(height: Spacing.s2);
+const gapH4 = SizedBox(height: Spacing.s4);
+const gapH6 = SizedBox(height: Spacing.s6);
+const gapH8 = SizedBox(height: Spacing.s8);
+const gapH10 = SizedBox(height: Spacing.s10);
+const gapH12 = SizedBox(height: Spacing.s12);
+const gapH14 = SizedBox(height: Spacing.s14);
+const gapH16 = SizedBox(height: Spacing.s16);
+const gapH20 = SizedBox(height: Spacing.s20);
+const gapH24 = SizedBox(height: Spacing.s24);
+const gapH28 = SizedBox(height: Spacing.s28);
+const gapH32 = SizedBox(height: Spacing.s32);
+const gapH36 = SizedBox(height: Spacing.s36);
+const gapH40 = SizedBox(height: Spacing.s40);
+const gapH44 = SizedBox(height: Spacing.s44);
+const gapH48 = SizedBox(height: Spacing.s48);
+const gapH56 = SizedBox(height: Spacing.s56);
+const gapH64 = SizedBox(height: Spacing.s64);
