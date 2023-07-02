6ce04554425fc8642337ff0fe7d879923df3ffeb
Bartek Kaczmarek <codebkme@gmail.com>
2023-07-02 11:30:22 +0200

    feat: add theme for new widgets

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -18,15 +18,30 @@ class App extends StatelessWidget {
           brightness: Brightness.light,
           seedColor: Colors.blue.shade900,
         ),
-        // appBarTheme: AppBarTheme(
-        //     //backgroundColor: NavigationBarTheme.of(context).backgroundColor,
-        //     ),
         navigationBarTheme: const NavigationBarThemeData(
           indicatorShape: CircleBorder(),
           labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
           height: 60,
         ),
         textTheme: GoogleFonts.latoTextTheme(),
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: BorderSide.none,
+          ),
+          filled: true,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            minimumSize: const Size.fromHeight(45),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+            shadowColor: Colors.transparent,
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            foregroundColor: Colors.white,
+          ),
+        ),
       ),
     );
   }
