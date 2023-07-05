65266de4f750dc96366bfb46601299872a42cc17
Bartek <codebkme@gmail.com>
2023-07-05 16:26:21 +0200

    feat: add common empty state widget

---
+++ lib/src/common/widgets/empty_state.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'package:shopit/src/constants/spacing.dart';
+
+class EmptyState extends StatelessWidget {
+  const EmptyState({super.key, required this.text, required this.onRefresh});
+
+  final String text;
+  final VoidCallback onRefresh;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SvgPicture.asset(
+            'assets/icons/information.svg',
+            width: 100,
+            colorFilter: ColorFilter.mode(
+              Theme.of(context).primaryColorDark.withAlpha(50),
+              BlendMode.srcIn,
+            ),
+          ),
+          Text(
+            text,
+            style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                  color: Theme.of(context).primaryColorDark.withAlpha(50),
+                ),
+            textAlign: TextAlign.center,
+          ),
+          gapH24,
+          IconButton(
+            onPressed: onRefresh,
+            icon: SvgPicture.asset(
+              'assets/icons/reload.svg',
+              colorFilter: ColorFilter.mode(
+                Theme.of(context).hintColor,
+                BlendMode.srcIn,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
