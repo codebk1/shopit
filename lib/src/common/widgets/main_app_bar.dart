6e27baa5e6360619516278bb259ec61c433dda2a
Bartek <codebkme@gmail.com>
2023-07-17 16:30:32 +0200

    feat: add app logo

--- lib/src/common/widgets/main_app_bar.dart
+++ lib/src/common/widgets/main_app_bar.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'package:shopit/src/features/cart/application/controllers/cart_controller.dart';
 
 class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
   const MainAppBar({
     super.key,
-    this.title = 'Shopit',
+    this.title,
     this.showActions = true,
     this.actions = const [],
   });
 
-  final String title;
+  final String? title;
   final bool showActions;
   final List<Widget> actions;
 
@@ -19,13 +22,29 @@ class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return AppBar(
       scrolledUnderElevation: 0,
-      title: Text(title),
+      title: title == null
+          ? SvgPicture.asset(
+              'assets/logo.svg',
+              height: 24,
+            )
+          : Text(title!),
       actions: showActions
           ? actions.isEmpty
               ? [
                   IconButton(
+                    padding: const EdgeInsets.only(right: 14),
                     onPressed: () => context.push('/cart'),
-                    icon: SvgPicture.asset('assets/icons/cart.svg'),
+                    icon: Badge(
+                      offset: const Offset(6, -4),
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      label: Consumer(
+                        builder: (context, ref, child) {
+                          final count = ref.watch(cartItemsCountProvider);
+                          return Text(count.toString());
+                        },
+                      ),
+                      child: SvgPicture.asset('assets/icons/cart.svg'),
+                    ),
                   )
                 ]
               : actions
