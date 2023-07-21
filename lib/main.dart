2506bd6b04e4493d9025686f78e9141f9086fa36
Bartek <codebkme@gmail.com>
2023-07-21 08:01:31 +0200

    chore: isar db initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
 
-import 'package:isar/isar.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:isar/isar.dart';
 import 'firebase_options.dart';
 
 import 'package:shopit/src/app.dart';
 import 'package:shopit/src/utils/isar_provider.dart';
 import 'package:shopit/src/features/account/domain/entities/account.dart';
+import 'package:shopit/src/utils/isar.dart';
+import 'package:shopit/src/features/cart/domain/schemas/local_cart.dart';
+import 'package:shopit/src/features/wishlist/domain/entities/wishlist.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   final dir = await getApplicationDocumentsDirectory();
-  final isar = await Isar.open([AccountSchema], directory: dir.path);
+  final isar = await Isar.open(
+    [WishlistSchema, LocalCartSchema],
+    directory: dir.path,
+  );
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
