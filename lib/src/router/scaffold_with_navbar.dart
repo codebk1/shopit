2c7b9bfc17b8b77f2cdfaa4afacaef34116803f3
Bartek <codebkme@gmail.com>
2023-07-30 11:24:17 +0200

    refactor: navbar icons

--- lib/src/router/scaffold_with_navbar.dart
+++ lib/src/router/scaffold_with_navbar.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
+import 'package:shopit/src/common/widgets/svg_icon.dart';
 import 'package:shopit/src/features/wishlist/application/controllers/wishlist_controller.dart';
 
 class ScaffoldWithNavBar extends StatelessWidget {
@@ -15,10 +15,10 @@ class ScaffoldWithNavBar extends StatelessWidget {
   final StatefulNavigationShell navigationShell;
 
   final _tabIcons = const [
-    'assets/icons/home.svg',
-    'assets/icons/categories.svg',
-    'assets/icons/heart.svg',
-    'assets/icons/account.svg',
+    'home',
+    'categories',
+    'heart',
+    'account',
   ];
 
   @override
@@ -35,9 +35,16 @@ class ScaffoldWithNavBar extends StatelessWidget {
           selectedIndex: navigationShell.currentIndex,
           destinations: _tabIcons.map((icon) {
             final iconIndex = _tabIcons.indexOf(icon);
-            return iconIndex == 2
-                ? NavigationDestination(
-                    icon: Badge(
+            final navIcon = SvgIcon(
+              iconName: icon,
+              color: navigationShell.currentIndex == iconIndex
+                  ? Theme.of(context).colorScheme.primary
+                  : Theme.of(context).colorScheme.secondary,
+            );
+
+            return NavigationDestination(
+              icon: iconIndex == 2
+                  ? Badge(
                       offset: const Offset(8, -8),
                       backgroundColor: Theme.of(context).colorScheme.primary,
                       label: Consumer(
@@ -49,30 +56,11 @@ class ScaffoldWithNavBar extends StatelessWidget {
                           return Text(wishlistCount.toString());
                         },
                       ),
-                      child: SvgPicture.asset(
-                        icon,
-                        colorFilter: ColorFilter.mode(
-                          navigationShell.currentIndex == iconIndex
-                              ? Theme.of(context).colorScheme.primary
-                              : Theme.of(context).colorScheme.secondary,
-                          BlendMode.srcIn,
-                        ),
-                      ),
-                    ),
-                    label: '',
-                  )
-                : NavigationDestination(
-                    icon: SvgPicture.asset(
-                      icon,
-                      colorFilter: ColorFilter.mode(
-                        navigationShell.currentIndex == iconIndex
-                            ? Theme.of(context).colorScheme.primary
-                            : Theme.of(context).colorScheme.secondary,
-                        BlendMode.srcIn,
-                      ),
-                    ),
-                    label: '',
-                  );
+                      child: navIcon,
+                    )
+                  : navIcon,
+              label: '',
+            );
           }).toList(),
         ),
       ),
