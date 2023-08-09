2283e28be502fa35eae8985b1a008db058a2f5ce
Bartek <codebkme@gmail.com>
2023-08-09 14:54:09 +0200

    test: add auth_repository and auth_firestore_datasource unit tests

---
+++ test/src/features/auth/data/datasources/auth_firestore_datasource_test.dart
@@ -0,0 +1,245 @@
+import 'dart:async';
+
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:mocktail/mocktail.dart';
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+
+import 'package:shopit/src/exceptions/auth_exception.dart';
+import 'package:shopit/src/features/auth/auth.dart';
+
+class MockFirebaseAuth extends Mock implements firebase_auth.FirebaseAuth {}
+
+class MockUserCredential extends Mock implements firebase_auth.UserCredential {
+  MockUserCredential() {
+    when(() => user).thenReturn(
+      MockFirebaseUser(uid: '1', email: 'test1@test.com'),
+    );
+  }
+}
+
+class MockFirebaseUser extends Mock implements firebase_auth.User {
+  MockFirebaseUser({
+    required this.uid,
+    required this.email,
+  });
+
+  @override
+  final String uid;
+
+  @override
+  final String email;
+}
+
+void main() {
+  const email = 'test1@test.com';
+  const password = 'test123';
+
+  const user1 = User(id: '1', email: 'test1@test.com');
+  const user2 = User(id: '2', email: 'test2@test.com');
+
+  final firebaseUser1 = MockFirebaseUser(uid: '1', email: 'test1@test.com');
+  final firebaseUser2 = MockFirebaseUser(uid: '2', email: 'test2@test.com');
+
+  group('AuthFirestoreDataSource', () {
+    late firebase_auth.FirebaseAuth firebaseAuth;
+    late AuthFirestoreDataSource sut;
+
+    setUp(() {
+      firebaseAuth = MockFirebaseAuth();
+      sut = AuthFirestoreDataSource(firebaseAuth);
+    });
+
+    group('currentUser', () {
+      test('should retrieve current user when available', () async {
+        expect(sut.currentUser, null);
+
+        when(() => firebaseAuth.currentUser).thenAnswer(
+          (_) => firebaseUser1,
+        );
+
+        expect(sut.currentUser, isA<User>());
+
+        verify(() => firebaseAuth.currentUser).called(2);
+      });
+    });
+
+    group('authStateChanges', () {
+      test('should emit user when available', () {
+        final controller = StreamController<MockFirebaseUser?>();
+
+        when(() => firebaseAuth.authStateChanges())
+            .thenAnswer((_) => controller.stream);
+
+        addTearDown(controller.close);
+
+        controller
+          ..add(firebaseUser1)
+          ..add(null)
+          ..add(firebaseUser2);
+
+        expect(sut.authStateChanges(), emitsInOrder([user1, null, user2]));
+      });
+    });
+
+    group('signInWithEmailAndPassword', () {
+      test(
+          'succeeds when FirebaseAuth signInWithEmailAndPassword method succeeds',
+          () {
+        when(() => firebaseAuth.signInWithEmailAndPassword(
+              email: email,
+              password: password,
+            )).thenAnswer((_) async => MockUserCredential());
+
+        expect(sut.signInWithEmailAndPassword(email, password), completes);
+      });
+
+      test('throws correct Exception when FirebaseAuthException is threw', () {
+        when(() => firebaseAuth.signInWithEmailAndPassword(
+              email: email,
+              password: password,
+            )).thenThrow(
+          firebase_auth.FirebaseAuthException(code: 'invalid-email'),
+        );
+
+        expect(
+          sut.signInWithEmailAndPassword(email, password),
+          throwsA(isA<InvalidEmailException>()),
+        );
+      });
+
+      test('throws correct Exception when FirebaseAuthException is not threw',
+          () {
+        when(() => firebaseAuth.signInWithEmailAndPassword(
+              email: email,
+              password: password,
+            )).thenThrow(Exception);
+
+        expect(
+          sut.signInWithEmailAndPassword(email, password),
+          throwsA(isA<AuthUnknownException>()),
+        );
+      });
+    });
+
+    group('signUpWithEmailAndPassword', () {
+      test(
+          'return User when FirebaseAuth createUserWithEmailAndPassword method returns UserCredential object',
+          () async {
+        when(() => firebaseAuth.createUserWithEmailAndPassword(
+              email: email,
+              password: password,
+            )).thenAnswer((_) async => MockUserCredential());
+
+        expect(
+          await sut.signUpWithEmailAndPassword(email, password),
+          user1,
+        );
+      });
+
+      test('throws correct Exception when FirebaseAuthException is threw', () {
+        when(() => firebaseAuth.createUserWithEmailAndPassword(
+              email: email,
+              password: password,
+            )).thenThrow(
+          firebase_auth.FirebaseAuthException(code: 'invalid-email'),
+        );
+
+        expect(
+          sut.signUpWithEmailAndPassword(email, password),
+          throwsA(isA<InvalidEmailException>()),
+        );
+      });
+
+      test('throws correct Exception when FirebaseAuthException is not threw',
+          () {
+        when(() => firebaseAuth.createUserWithEmailAndPassword(
+              email: email,
+              password: password,
+            )).thenThrow(Exception);
+
+        expect(
+          sut.signUpWithEmailAndPassword(email, password),
+          throwsA(isA<AuthUnknownException>()),
+        );
+      });
+    });
+
+    group('signOut', () {
+      test('succeeds when FirebaseAuth signOut method succeeds', () {
+        when(firebaseAuth.signOut).thenAnswer((_) async => Future<void>);
+        expect(sut.signOut(), completes);
+      });
+    });
+
+    group('updateEmail', () {
+      test('succeeds when FirebaseAuth updateEmail method succeeds', () {
+        when(() => firebaseAuth.currentUser).thenAnswer(
+          (_) => firebaseUser1,
+        );
+
+        when(() => firebaseUser1.updateEmail(email))
+            .thenAnswer((_) async => Future<void>);
+
+        expect(sut.updateEmail(email), completes);
+      });
+
+      test('throws correct Exception when FirebaseAuthException is threw', () {
+        when(() => firebaseAuth.currentUser!.updateEmail(email)).thenThrow(
+          firebase_auth.FirebaseAuthException(code: 'invalid-email'),
+        );
+
+        expect(
+          sut.updateEmail(email),
+          throwsA(isA<InvalidEmailException>()),
+        );
+      });
+
+      test('throws correct Exception when FirebaseAuthException is not threw',
+          () {
+        when(() => firebaseAuth.currentUser!.updateEmail(email))
+            .thenThrow(Exception);
+
+        expect(
+          sut.updateEmail(email),
+          throwsA(isA<AuthUnknownException>()),
+        );
+      });
+    });
+
+    group('updatePassword', () {
+      test('succeeds when FirebaseAuth updatePassword method succeeds', () {
+        when(() => firebaseAuth.currentUser).thenAnswer(
+          (_) => firebaseUser1,
+        );
+
+        when(() => firebaseUser1.updatePassword(password))
+            .thenAnswer((_) async => Future<void>);
+
+        expect(sut.updatePassword(password), completes);
+      });
+
+      test('throws correct Exception when FirebaseAuthException is threw', () {
+        when(() => firebaseAuth.currentUser!.updateEmail(email)).thenThrow(
+          firebase_auth.FirebaseAuthException(code: 'weak-password'),
+        );
+
+        expect(
+          sut.updatePassword(password),
+          throwsA(isA<WeakPasswordException>()),
+        );
+      });
+
+      test('throws correct Exception when FirebaseAuthException is not threw',
+          () {
+        when(() => firebaseAuth.currentUser!.updatePassword(password))
+            .thenThrow(Exception);
+
+        expect(
+          sut.updatePassword(password),
+          throwsA(isA<AuthUnknownException>()),
+        );
+      });
+    });
+  });
+}
---
+++ test/src/features/auth/data/repositories/auth_repository_test.dart
@@ -0,0 +1,209 @@
+import 'dart:async';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import 'package:shopit/src/features/auth/auth.dart';
+
+class FakeUser extends Fake implements User {
+  FakeUser({
+    required this.id,
+    required this.email,
+  });
+
+  @override
+  final String id;
+
+  @override
+  final String email;
+}
+
+class MockUser extends Mock implements User {
+  MockUser() {
+    when(() => id).thenReturn('1');
+    when(() => email).thenReturn('test@test.com');
+  }
+}
+
+class MockIAuthRemoteDataSource extends Mock implements IAuthRemoteDataSource {}
+
+void main() {
+  const email = 'test@test.com';
+  const password = 'test123';
+
+  group('AuthRepository', () {
+    late IAuthRemoteDataSource remoteDataSource;
+    late AuthRepository sut;
+
+    setUp(() {
+      remoteDataSource = MockIAuthRemoteDataSource();
+      sut = AuthRepository(remoteDataSource);
+    });
+
+    group('currentUser', () {
+      test('should retrieve current user when available', () async {
+        expect(sut.currentUser, null);
+
+        when(() => remoteDataSource.currentUser).thenAnswer((_) => MockUser());
+
+        expect(sut.currentUser, isA<MockUser>());
+
+        verify(() => remoteDataSource.currentUser).called(2);
+      });
+    });
+
+    group('authStateChanges', () {
+      test('should react to remoteDataSource auth state changes', () async {
+        final controller = StreamController<FakeUser?>();
+
+        when(() => remoteDataSource.authStateChanges())
+            .thenAnswer((_) => controller.stream);
+
+        addTearDown(controller.close);
+
+        final user1 = FakeUser(id: '1', email: 'test1@gmail.com');
+        final user2 = FakeUser(id: '2', email: 'test2@gmail.com');
+
+        controller
+          ..add(user1)
+          ..add(user2)
+          ..add(null);
+
+        expect(
+          sut.authStateChanges(),
+          emitsInOrder([user1, user2, null]),
+        );
+      });
+    });
+
+    group('signIn', () {
+      setUp(() {
+        when(() => remoteDataSource.signInWithEmailAndPassword(email, password))
+            .thenAnswer((_) async => Future<void>);
+      });
+
+      test('calls remoteDataSource', () async {
+        await sut.signIn(email, password);
+
+        verify(
+          () => remoteDataSource.signInWithEmailAndPassword(email, password),
+        ).called(1);
+      });
+
+      test('succeeds when remoteDataSource succeeds', () async {
+        expect(sut.signIn(email, password), completes);
+      });
+
+      test('throws when remoteDataSource throws', () async {
+        when(() => remoteDataSource.signInWithEmailAndPassword(email, password))
+            .thenThrow(Exception());
+
+        expect(
+          () => sut.signIn(email, password),
+          throwsA(isA<Exception>()),
+        );
+      });
+    });
+
+    group('signUp', () {
+      setUp(() {
+        when(() => remoteDataSource.signUpWithEmailAndPassword(email, password))
+            .thenAnswer((_) async => MockUser());
+      });
+
+      test('calls remoteDataSource', () async {
+        await sut.signUp(email, password);
+
+        verify(
+          () => remoteDataSource.signUpWithEmailAndPassword(email, password),
+        ).called(1);
+      });
+
+      test('succeeds when remoteDataSource succeeds', () async {
+        expect(sut.signUp(email, password), completes);
+      });
+
+      test('throws when remoteDataSource throws', () async {
+        when(() => remoteDataSource.signUpWithEmailAndPassword(email, password))
+            .thenThrow(Exception());
+
+        expect(
+          () => sut.signUp(email, password),
+          throwsA(isA<Exception>()),
+        );
+      });
+    });
+
+    group('signOut', () {
+      setUp(() {
+        when(remoteDataSource.signOut).thenAnswer((_) async => Future<void>);
+      });
+
+      test('calls remoteDataSource', () async {
+        await sut.signOut();
+        verify(remoteDataSource.signOut).called(1);
+      });
+
+      test('succeeds when remoteDataSource succeeds', () async {
+        expect(sut.signOut(), completes);
+      });
+    });
+
+    group('updateEmail', () {
+      setUp(() {
+        when(() => remoteDataSource.updateEmail(email))
+            .thenAnswer((_) async => Future<void>);
+      });
+
+      test('calls remoteDataSource', () async {
+        await sut.updateEmail(email);
+
+        verify(
+          () => remoteDataSource.updateEmail(email),
+        ).called(1);
+      });
+
+      test('succeeds when remoteDataSource succeeds', () async {
+        expect(sut.updateEmail(email), completes);
+      });
+
+      test('throws when remoteDataSource throws', () async {
+        when(() => remoteDataSource.updateEmail(email)).thenThrow(Exception());
+
+        expect(
+          () => sut.updateEmail(email),
+          throwsA(isA<Exception>()),
+        );
+      });
+    });
+
+    group('updatePassword', () {
+      setUp(() {
+        when(() => remoteDataSource.updatePassword(password))
+            .thenAnswer((_) async => Future<void>);
+      });
+
+      test('calls remoteDataSource', () async {
+        await sut.updatePassword(password);
+
+        verify(
+          () => remoteDataSource.updatePassword(password),
+        ).called(1);
+      });
+
+      test('succeeds when remoteDataSource succeeds', () async {
+        expect(sut.updatePassword(password), completes);
+      });
+
+      test('throws when remoteDataSource throws', () async {
+        when(() => remoteDataSource.updatePassword(password))
+            .thenThrow(Exception());
+
+        expect(
+          () => sut.updatePassword(password),
+          throwsA(isA<Exception>()),
+        );
+      });
+    });
+  });
+}
