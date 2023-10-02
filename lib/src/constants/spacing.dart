8f7bc419af74692b276c88dcd0e731f185399ac0
Bartek <codebkme@gmail.com>
2023-10-02 09:25:12 +0200

    feat: add sliver spacing helpers

--- lib/src/constants/spacing.dart
+++ lib/src/constants/spacing.dart
@@ -63,3 +63,45 @@ const gapH44 = SizedBox(height: Spacing.s44);
 const gapH48 = SizedBox(height: Spacing.s48);
 const gapH56 = SizedBox(height: Spacing.s56);
 const gapH64 = SizedBox(height: Spacing.s64);
+
+// sliver horizontal spacing
+const sliverGapW1 = SliverToBoxAdapter(child: SizedBox(width: Spacing.s1));
+const sliverGapW2 = SliverToBoxAdapter(child: SizedBox(width: Spacing.s2));
+const sliverGapW4 = SliverToBoxAdapter(child: SizedBox(width: Spacing.s4));
+const sliverGapW6 = SliverToBoxAdapter(child: SizedBox(width: Spacing.s6));
+const sliverGapW8 = SliverToBoxAdapter(child: SizedBox(width: Spacing.s8));
+const sliverGapW10 = SliverToBoxAdapter(child: SizedBox(width: Spacing.s10));
+const sliverGapW12 = SliverToBoxAdapter(child: SizedBox(width: Spacing.s12));
+const sliverGapW14 = SliverToBoxAdapter(child: SizedBox(width: Spacing.s14));
+const sliverGapW16 = SliverToBoxAdapter(child: SizedBox(width: Spacing.s16));
+const sliverGapW20 = SliverToBoxAdapter(child: SizedBox(width: Spacing.s20));
+const sliverGapW24 = SliverToBoxAdapter(child: SizedBox(width: Spacing.s24));
+const sliverGapW28 = SliverToBoxAdapter(child: SizedBox(width: Spacing.s28));
+const sliverGapW32 = SliverToBoxAdapter(child: SizedBox(width: Spacing.s32));
+const sliverGapW36 = SliverToBoxAdapter(child: SizedBox(width: Spacing.s36));
+const sliverGapW40 = SliverToBoxAdapter(child: SizedBox(width: Spacing.s40));
+const sliverGapW44 = SliverToBoxAdapter(child: SizedBox(width: Spacing.s44));
+const sliverGapW48 = SliverToBoxAdapter(child: SizedBox(width: Spacing.s48));
+const sliverGapW56 = SliverToBoxAdapter(child: SizedBox(width: Spacing.s56));
+const sliverGapW64 = SliverToBoxAdapter(child: SizedBox(width: Spacing.s64));
+
+// sliver vertical spacing
+const sliverGapH1 = SliverToBoxAdapter(child: SizedBox(height: Spacing.s1));
+const sliverGapH2 = SliverToBoxAdapter(child: SizedBox(height: Spacing.s2));
+const sliverGapH4 = SliverToBoxAdapter(child: SizedBox(height: Spacing.s4));
+const sliverGapH6 = SliverToBoxAdapter(child: SizedBox(height: Spacing.s6));
+const sliverGapH8 = SliverToBoxAdapter(child: SizedBox(height: Spacing.s8));
+const sliverGapH10 = SliverToBoxAdapter(child: SizedBox(height: Spacing.s10));
+const sliverGapH12 = SliverToBoxAdapter(child: SizedBox(height: Spacing.s12));
+const sliverGapH14 = SliverToBoxAdapter(child: SizedBox(height: Spacing.s14));
+const sliverGapH16 = SliverToBoxAdapter(child: SizedBox(height: Spacing.s16));
+const sliverGapH20 = SliverToBoxAdapter(child: SizedBox(height: Spacing.s20));
+const sliverGapH24 = SliverToBoxAdapter(child: SizedBox(height: Spacing.s24));
+const sliverGapH28 = SliverToBoxAdapter(child: SizedBox(height: Spacing.s28));
+const sliverGapH32 = SliverToBoxAdapter(child: SizedBox(height: Spacing.s32));
+const sliverGapH36 = SliverToBoxAdapter(child: SizedBox(height: Spacing.s36));
+const sliverGapH40 = SliverToBoxAdapter(child: SizedBox(height: Spacing.s40));
+const sliverGapH44 = SliverToBoxAdapter(child: SizedBox(height: Spacing.s44));
+const sliverGapH48 = SliverToBoxAdapter(child: SizedBox(height: Spacing.s48));
+const sliverGapH56 = SliverToBoxAdapter(child: SizedBox(height: Spacing.s56));
+const sliverGapH64 = SliverToBoxAdapter(child: SizedBox(height: Spacing.s64));
