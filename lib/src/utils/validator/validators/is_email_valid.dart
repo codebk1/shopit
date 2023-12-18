6a10365021927f2b310f86070bf2fd890e041670
Bartek <codebkme@gmail.com>
2023-12-18 12:32:55 +0100

    fix: remove unused file

--- lib/src/utils/validator/validators/is_email_valid.dart
+++
@@ -1,7 +0,0 @@
-String? isEmailValid(String? value, String errorMessage) {
-  final regExp = RegExp(
-    r'^[a-zA-Z\d.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z\d-]+(?:\.[a-zA-Z\d-]+)*$',
-  );
-
-  return regExp.hasMatch(value!) ? null : errorMessage;
-}
